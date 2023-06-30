06aeae87c42878b70692b587d2a129ec58d99c9e
sebetc4 <seb-etc@hotmail.fr>
2023-06-30 15:09:31 +0200

    feat: add script ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,13 +5,12 @@ const host = process.env.ECOSYSTEM_HOST;
 const ref = process.env.ECOSYSTEM_REF;
 const repo = process.env.ECOSYSTEM_REPO;
 const path = process.env.ECOSYSTEM_PATH;
-const script = process.env.ECOSYSTEM_APPS_SCRIPT;
 const postDeploy = process.env.ECOSYSTEM_POST_DEPLOY;
 
 module.exports = {
     apps: [
         {
-            script,
+            script: 'pnpm run start',
             watch: '.',
         },
     ],
